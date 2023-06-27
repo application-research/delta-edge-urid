c3eb4696a5cc9e323220b29355167053f9a820ac
alvin-reyes <me@areyes.onl>
2023-06-27 18:05:07 -0400

    add tag name query on status checl

--- api/status_check.go
+++ api/status_check.go
@@ -22,7 +22,7 @@ type StatusCheckResponse struct {
 }
 
 func ConfigureStatusCheckRouter(e *echo.Group, node *core.LightNode) {
-	e.GET("/status/cid/:id", func(c echo.Context) error {
+	e.GET("/status/cid/:cid", func(c echo.Context) error {
 
 		authorizationString := c.Request().Header.Get("Authorization")
 		authParts := strings.Split(authorizationString, " ")
@@ -77,6 +77,62 @@ func ConfigureStatusCheckRouter(e *echo.Group, node *core.LightNode) {
 
 		var bucket core.Bucket
 		node.DB.Model(&core.Bucket{}).Where("requesting_api_key = ? and uuid = ?", authParts[1], c.Param("uuid")).Scan(&bucket)
+
+		// get the cid
+		bucketCid, err := cid.Decode(bucket.Cid)
+		if err != nil {
+			return c.JSON(404, map[string]interface{}{
+				"message": "Bucket not found. Please check if you have the proper API key or if the bucket id is valid",
+			})
+		}
+		dirNdRaw, err := node.Node.DAGService.Get(context.Background(), bucketCid)
+		if err != nil {
+			return c.JSON(404, map[string]interface{}{
+				"message": "Bucket not found. Please check if you have the proper API key or if the bucket id is valid",
+			})
+		}
+
+		var contents []core.Content
+		node.DB.Model(&core.Content{}).Where("requesting_api_key = ? and bucket_uuid = ?", authParts[1], c.Param("uuid")).Scan(&contents)
+
+		var contentResponse []core.Content
+		for _, content := range contents {
+			content.RequestingApiKey = ""
+
+			for _, link := range dirNdRaw.Links() {
+				cidFromDb, err := cid.Decode(content.Cid)
+				if err != nil {
+					continue
+				}
+				if link.Cid == cidFromDb {
+					content.Cid = link.Cid.String()
+					content.RequestingApiKey = ""
+					contentResponse = append(contentResponse, content)
+				}
+
+			}
+			//job := jobs.CreateNewDispatcher()
+			//job.AddJob(jobs.NewDealItemChecker(node, content))
+
+		}
+		// trigger status check
+		job := jobs.CreateNewDispatcher()
+		//job.AddJob(jobs.NewCarDealItemChecker(node, bucket))
+		job.Start(len(contents) + 1)
+
+		bucket.RequestingApiKey = ""
+		return c.JSON(200, map[string]interface{}{
+			"bucket":        bucket,
+			"content_links": contentResponse,
+		})
+	})
+	e.GET("/status/tag/:tag-name", func(c echo.Context) error {
+
+		authorizationString := c.Request().Header.Get("Authorization")
+		authParts := strings.Split(authorizationString, " ")
+
+		var bucket core.Bucket
+		node.DB.Model(&core.Bucket{}).Where("requesting_api_key = ? and tag_name = ?", authParts[1], c.Param("tag-name")).Scan(&bucket)
 
 		// get the cid
 		bucketCid, err := cid.Decode(bucket.Cid)
